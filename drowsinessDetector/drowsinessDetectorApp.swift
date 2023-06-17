1bcb71be481bc0c0dc2af4f13eeec6a4f62560e5
Igor Silva <igorsilvadeveloper@gmail.com>
2023-06-17 15:03:20 -0300

    Initial Commit

---
+++ drowsinessDetector/drowsinessDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  drowsinessDetectorApp.swift
+//  drowsinessDetector
+//
+//  Created by Igor Silva on 17/06/23.
+//
+
+import SwiftUI
+
+@main
+struct drowsinessDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
